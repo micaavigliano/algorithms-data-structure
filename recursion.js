21e9f4aae78b440ed3e2b1b60385ce962847a806
MicaAvigliano <micaela.avigliano@gmail.com>
2024-05-12 18:29:41 -0300

    recursion 2

--- recursion.js
+++ recursion.js
@@ -155,7 +155,62 @@ function reverse(str) {
 }
 
 // Example usage:
-console.log(reverse("hello")); // Output: "olleh"
-console.log(reverse("world")); // Output: "dlrow"
-console.log(reverse("")); // Output: ""
-console.log(reverse("a")); // Output: "a"
+// console.log(reverse("hello")); // Output: "olleh"
+// console.log(reverse("world")); // Output: "dlrow"
+// console.log(reverse("")); // Output: ""
+// console.log(reverse("a")); // Output: "a"
+
+// 5. isPalindrome: write a recursive function which returns trye if the string passed to it is a palindrome. Otherwise, is returns false.
+
+const isPalindrome = (str) => {
+  if (str.length === 1) return true;
+  if (str.length === 2) return str[0] === str[1];
+  if (str[0] === str.slice(-1)) return isPalindrome(str.slice(1, -1));
+  return false;
+};
+
+//console.log(isPalindrome("awesome")); // false
+// isPalindrome('foobar') // false
+//console.log(isPalindrome("tacocat")); // true
+// isPalindrome('amanaplanacanalpanama') // true
+// isPalindrome('amanaplanacanalpandemonium') // false
+
+// 6. someRecursive: write a function which accepts an array and a callback. the function returns true if a single value in the array returns true when passed to the callback. Otherwise it returns false
+
+const someRecursive = (array, callback) => {
+  if (array.length === 0) return false;
+  if (callback(array[0])) return true;
+  return someRecursive(array.slice(1), callback);
+};
+
+//const isOdd = (val) => val % 2 !== 0;
+
+//console.log(someRecursive([1, 2, 3, 4], isOdd)); // true
+// someRecursive([4,6,8,9], isOdd) // true
+//console.log(someRecursive([4, 6, 8], isOdd)); // false
+// someRecursive([4,6,8], val => val > 10); // false
+
+// 7. isFlatten write a recursive function which accepts an array of arrays with all values flattened
+
+const flatten = (array) => {
+  let newArr = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      newArr = newArr.concat(flatten(array[i]));
+    } else {
+      newArr.push(array[i]);
+    }
+  }
+
+  return newArr;
+};
+
+//console.log(flatten([1, 2, 3, [4, 5]])); // [1, 2, 3, 4, 5]
+// console.log(flatten([1, [2, [3, 4], [[5]]]])) // [1, 2, 3, 4, 5]
+// console.log(flatten([[1],[2],[3]])) // [1,2,3]
+//console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])); // [1,2,3
+
+// 8. capitalizeFirst: write a recursion function. Given an arrat of strings, capitalize the first letter of each string in the array
+
+const capitalizeFirst = (arr) => {};
